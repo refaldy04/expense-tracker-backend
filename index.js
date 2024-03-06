d4b98b3e6957d02caff5dbe3ff216f79f40b6f58
Refaldy <refaldybanggana@gmail.com>
2024-03-06 19:59:44 +0700

    setup deployment

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ await server.start();
 app.use(
   "/graphql",
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://expense-tracker-frontend-jade.vercel.app",
     credentials: true,
   }),
   express.json(),
