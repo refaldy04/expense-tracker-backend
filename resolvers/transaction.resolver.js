d9ed8b5ef24d1270345e94aa1bd52b2ea3fe41c2
Refaldy <refaldybanggana@gmail.com>
2024-03-06 16:14:09 +0700

    create query for transaction statistic

--- resolvers/transaction.resolver.js
+++ resolvers/transaction.resolver.js
@@ -24,6 +24,28 @@ const transactionResolver = {
         throw new Error(err.message || "Error getting transaction");
       }
     },
+
+    categoryStatistics: async (_, __, context) => {
+      if (!context.getUser()) throw new Error("Unauthorized");
+
+      const userId = context.getUser()._id;
+      const transactions = await Transaction.find({
+        userId,
+      });
+      const categoryMap = {};
+
+      transactions.forEach((transaction) => {
+        if (!categoryMap[transaction.category]) {
+          categoryMap[transaction.category] = 0;
+        }
+        categoryMap[transaction.category] += transaction.amount;
+      });
+
+      return Object.entries(categoryMap).map(([category, totalAmount]) => ({
+        category,
+        totalAmount,
+      }));
+    },
   },
   Mutation: {
     createTransaction: async (_, { input }, context) => {
