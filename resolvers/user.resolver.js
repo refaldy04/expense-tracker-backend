e4b9cdd12a95d42b139b1de2333efa39578af565
Refaldy <refaldybanggana@gmail.com>
2024-03-07 09:32:17 +0700

    debug

--- resolvers/user.resolver.js
+++ resolvers/user.resolver.js
@@ -42,11 +42,14 @@ const userResolver = {
     login: async (_, { input }, context) => {
       try {
         const { username, password } = input;
+        console.log(input);
         if (!username || !password) throw new Error("All fields are required");
         const { user } = await context.authenticate("graphql-local", {
           username,
           password,
         });
+
+        console.log(user);
 
         await context.login(user);
         return user;
